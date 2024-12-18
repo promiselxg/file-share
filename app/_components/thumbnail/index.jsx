d6c681ab913478814e42cd85a18b27e434e07c15
promiselxg <okeydeede@gmail.com>
2024-12-18 06:46:45 +0100

    addd context menu

--- app/_components/thumbnail/index.jsx
+++ app/_components/thumbnail/index.jsx
@@ -8,53 +8,73 @@ import React from "react";
 import { FiMoreHorizontal, FiVideo } from "react-icons/fi";
 import { LinkWithIcon } from "../nav/sideBarNav";
 import { PiShareFatLight } from "react-icons/pi";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 
 const ThumbNail = ({ title, date, img, time }) => {
   return (
-    <div className="">
-      <div className="w-full overflow-hidden h-[200px] rounded-[8px] border border-[--folder-border-color] relative imgThubnail  cursor-pointer">
-        <Image
-          src={
-            img ??
-            "https://awevideo.s3.amazonaws.com/video-24223306-7d6baf003965db122db057d26116cf61_960x540.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20241218%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241218T044836Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=feb3094abb0ff6e7e04102d7186a2fd4d104c4c432cb8ae2b1fec14204a2438a"
-          }
-          width={500}
-          height={200}
-          alt="video thumbnail"
-          className="w-full object-cover h-[200px]"
-        />
-        <div className="absolute bottom-5 right-2 bg-[rgba(0,0,0,.8)] text-white text-sm py-[2px] px-[8px] rounded-[5px] font-[600]">
-          {time}
+    <ContextMenu className="flex flex-col">
+      {/* Context Menu Trigger */}
+      <ContextMenuTrigger>
+        <div className="w-full overflow-hidden h-[200px] rounded-[8px] border border-[--folder-border-color] relative imgThubnail  cursor-pointer">
+          <Image
+            src={
+              img ??
+              "https://awevideo.s3.amazonaws.com/video-24223306-7d6baf003965db122db057d26116cf61_960x540.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20241218%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241218T044836Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=feb3094abb0ff6e7e04102d7186a2fd4d104c4c432cb8ae2b1fec14204a2438a"
+            }
+            width={500}
+            height={200}
+            alt="video thumbnail"
+            className="w-full object-cover h-[200px]"
+          />
+          {time && (
+            <div className="absolute bottom-5 right-2 bg-[rgba(0,0,0,.8)] text-white text-sm py-[2px] px-[8px] rounded-[5px] font-[600]">
+              {time}
+            </div>
+          )}
+          <div className="link-transition">
+            <div className="w-full relative top-0 bottom-0 bg-[rgba(0,0,0,.5)] link-transition bg-overlay"></div>
+            <Popover className="w-full">
+              <PopoverTrigger className="flex items-center gap-[4px] text-[--sidebar-link-color] text-[14px] absolute top-0 right-0">
+                <span className="hover:bg-[--folder-bg] p-2 rounded-[8px] horizontalIcon opacity-0">
+                  <FiMoreHorizontal size={20} className="text-white" />
+                </span>
+              </PopoverTrigger>
+              <PopoverContent className="flex bg-[--dialog-bg] shadow-md border-none w-[220px] absolute right-5 top-0 z-10">
+                <ul className="flex flex-col w-full">
+                  <li className="flex w-full text-[--sidebar-link-active-text] hover:bg-[--folder-bg] rounded-[5px] link-transition">
+                    <LinkWithIcon
+                      Icon={<PiShareFatLight size={20} />}
+                      name="Share"
+                    />
+                  </li>
+                </ul>
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
-        <div className="link-transition">
-          <div className="w-full relative top-0 bottom-0 bg-[rgba(0,0,0,.5)] link-transition bg-overlay"></div>
-          <Popover className="w-full">
-            <PopoverTrigger className="flex items-center gap-[4px] text-[--sidebar-link-color] text-[14px] absolute top-0 right-0">
-              <span className="hover:bg-[--folder-bg]  p-2 rounded-[8px] horizontalIcon opacity-0">
-                <FiMoreHorizontal size={20} className="text-white" />
-              </span>
-            </PopoverTrigger>
-            <PopoverContent className="flex bg-[--dialog-bg] shadow-md border-none w-[220px] absolute right-5 top-0 z-10">
-              <ul className="flex flex-col w-full">
-                <li className="flex w-full text-[--sidebar-link-active-text] hover:bg-[--folder-bg]  rounded-[5px] link-transition">
-                  <LinkWithIcon
-                    Icon={<PiShareFatLight size={20} />}
-                    name="Share"
-                  />
-                </li>
-              </ul>
-            </PopoverContent>
-          </Popover>
+        {/* Thumbnail Title and Date */}
+        <div className="py-2 text-[--gray]">
+          <h1 className="text-white text-sm leading-tight">{title}</h1>
+          <div className="flex items-center gap-2 mt-1">
+            <FiVideo size={14} />
+            <span className="text-[--gray] text-[12px]">{date}</span>
+          </div>
         </div>
-      </div>
-      <div className="py-2 text-[--gray]">
-        <h1 className="text-white text-sm leading-tight">{title}</h1>
-        <div className="flex items-center gap-2 mt-1">
-          <FiVideo size={14} />
-          <span className="text-[--gray] text-[12px]">{date}</span>
-        </div>
-      </div>
-    </div>
+      </ContextMenuTrigger>
+
+      {/* Context Menu Content */}
+      <ContextMenuContent>
+        <ContextMenuItem>Profile</ContextMenuItem>
+        <ContextMenuItem>Billing</ContextMenuItem>
+        <ContextMenuItem>Team</ContextMenuItem>
+        <ContextMenuItem>Subscription</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
 
--- app/my_items/page.jsx
+++ app/my_items/page.jsx
@@ -133,14 +133,23 @@ export default function Home() {
                   date="Jan 21, 2024"
                   time="00:24"
                 />
-                <ThumbNail title="Create next App" date="Jan 22, 2024" />
                 <ThumbNail
-                  title="Glovo: you order, we deliver it!"
-                  date="Jan 21, 2024"
+                  title="Vite + React"
+                  date="Jan 22, 2024"
+                  time="01:05"
+                  img="https://awevideo.s3.amazonaws.com/video-20660451-062988d3917c276b4ffed4f35c16b0dd_960x540.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20241218%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241218T053037Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=2ba9b5886bb0e0f759c7408218740066fe931d05676a45f1ec718e22baecaf6d"
                 />
                 <ThumbNail
-                  title="Glovo: you order, we deliver it!"
+                  title="Transactions | frontend"
                   date="Jan 21, 2024"
+                  time="00:08"
+                  img="https://awevideo.s3.amazonaws.com/video-19844464-fa3c35cd3dc7e46dace12bf447110ce5_960x540.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20241218%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241218T053130Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=0bf56a79148acb972d6aefd3af9365bd6fa24e5bdb921a100392eb492c24a8ed"
+                />
+                <ThumbNail
+                  title="localhost:3000"
+                  date="Jan 21, 2024"
+                  img="https://awevideo.s3.amazonaws.com/video-10611774-57efe14ad3ae9407765838011ab4607a_960x540.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20241218%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241218T053230Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=42f94d75ac6018dd7863f4a9b6af500b47de058b7d62e59a3c37bdde4f0887e1"
+                  time="00:23"
                 />
               </div>
             </div>
