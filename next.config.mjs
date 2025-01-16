0810d5d770b62156df9cab0c5150d74fcb4fd53d
promiselxg <okeydeede@gmail.com>
2025-01-17 00:20:57 +0100

    added page redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/my_items",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
